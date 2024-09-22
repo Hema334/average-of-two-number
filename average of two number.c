956a175a8fa5eb37e1c363319d96afe44adaeaca
Hema334 <112267828+Hema334@users.noreply.github.com>
2024-09-22 12:14:51 +0530

    Add files via upload

---
+++ average of two number.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+int num1,num2,result;
+num1=42;
+num2=36;
+result=(num1+num2);
+printf("the average of two numbers is%d",result);
+}
