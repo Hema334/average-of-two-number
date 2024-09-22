eb229bee03beac64a61f82b64ffdd1db7df67146
Hema334 <112267828+Hema334@users.noreply.github.com>
2024-09-22 12:21:23 +0530

    Add files via upload

---
+++ average of three number.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+int num1,num2,num3,result;
+num1=42;
+num2=36;
+num3=27;
+result=(num1+num2+num3)/2;
+printf("the average of two numbers is%d",result);
+}
